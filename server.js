dc32a2bb95955688ba7c94235376cd8918431545
DwayneDevelops <DwayneDevelops@gmail.com>
2019-05-29 18:18:19 -0500

    still working on mvp

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express = 'express';
+const express = require('express');
 const helmet = require('helmet');
 
-
 const server = express();
+const postRouter = require('./posts/postRouter');
+const userRouter = require('./users/userRouter');
 
-server.use(express.json(), helmet(), logger)
+server.use(express.json(), helmet(), logger);
+server.use("/api/users", userRouter);
+server.use("/api/posts", postRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
@@ -13,7 +16,7 @@ server.get('/', (req, res) => {
 //custom middleware
 
 function logger(req, res, next) {
-  console.log(`A ${req.method} request to '${req.url}' at '${Date.now().toISOString()}`);
+  console.log(`A ${req.method} request to '${req.url}' at '${Date.now()}`);
   next();
 };
 
